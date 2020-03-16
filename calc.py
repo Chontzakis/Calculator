1ea36a74c17a29bf803a640672308f8ccad665e2
DChontzakis <55055493+Chontzakis@users.noreply.github.com>
2020-03-16 20:25:11 +0200

    only changed the way buttons look

--- calc.py
+++ calc.py
@@ -1,21 +1,38 @@
 from tkinter import *
 
 window = Tk()
-window.geometry('300x500+300+300')
 window.configure(background = 'black')
-numbers=[]
-numbers.append(Button(window,text='1',width='11').grid(row=4,column=2))
-numbers.append(Button(window,text='2',width='11').grid(row=4,column=1))
-numbers.append(Button(window,text='3',width='11').grid(row=4,column=0))
-numbers.append(Button(window,text='4',width='11').grid(row=3,column=2))
-numbers.append(Button(window,text='5',width='11').grid(row=3,column=1))
-numbers.append(Button(window,text='6',width='11').grid(row=3,column=0))
-numbers.append(Button(window,text='7',width='11').grid(row=2,column=2))
-numbers.append(Button(window,text='8',width='11').grid(row=2,column=1))
-numbers.append(Button(window,text='9',width='11').grid(row=2,column=0))
-signs = []
-signs.append(Button(window,text='+',width='11').grid(row=5,column=0)) #0
-signs.append(Button(window,text='*',width='11').grid(row=5,column=1)) #1
-signs.append(Button(window,text='/',width='11').grid(row=5,column=2)) #2
-signs.append(Button(window,text='C',width='35').grid(row=6,columnspan=5)) #3
+top_label = Entry(window).grid(row=0,columnspan=3)
+def button_click(number):
+    top_label.insert(0,number)
+
+n1 = Button(window,text='1',width='11',padx = 12 , pady = 10,command = lambda: button_click(1))
+n2 = Button(window,text='2',width='11',padx = 12 , pady = 10,command = lambda: button_click(2))
+n3 = Button(window,text='3',width='11',padx = 12 , pady = 10,command = lambda: button_click(3))
+n4 = Button(window,text='4',width='11',padx = 12 , pady = 10,command = lambda: button_click(4))
+n5 = Button(window,text='5',width='11',padx = 12 , pady = 10,command = lambda: button_click(5))
+n6 = Button(window,text='6',width='11',padx = 12 , pady = 10,command = lambda: button_click(6))
+n7 = Button(window,text='7',width='11',padx = 12 , pady = 10,command = lambda: button_click(7))
+n8 = Button(window,text='8',width='11',padx = 12 , pady = 10,command = lambda: button_click(8))
+n9 = Button(window,text='9',width='11',padx = 12 , pady = 10,command = lambda: button_click(9))
+n1.grid(row=4,column=2)
+n2.grid(row=4,column=1)
+n3.grid(row=4,column=0)
+n4.grid(row=3,column=2)
+n5.grid(row=3,column=1)
+n6.grid(row=3,column=0)
+n7.grid(row=2,column=2)
+n8.grid(row=2,column=1)
+n9.grid(row=2,column=0)
+
+s1 = Button(window,text='+',width='11',padx = 12 , pady = 10)
+s2 = Button(window,text='*',width='11',padx = 12 , pady = 10)
+s3 = Button(window,text='/',width='11',padx = 12 , pady = 10)
+s4 = Button(window,text='C',width='40',padx = 10 , pady = 10)
+s1.grid(row=5,column=0) #0
+s2.grid(row=5,column=1) #1
+s3.grid(row=5,column=2) #2
+s4.grid(row=6,columnspan=3)
+
+
 window.mainloop()
